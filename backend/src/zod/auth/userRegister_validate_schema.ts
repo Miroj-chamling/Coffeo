b8d5e0dbdd98d993676b8269657fc19621818995
mirojj <raimiroj7@gmail.com>
2024-08-09 10:06:49 +0545

    test change

--- backend/src/zod/auth/userRegister_validate_schema.ts
+++ backend/src/zod/auth/userRegister_validate_schema.ts
@@ -3,5 +3,5 @@ import {z} from "zod";
 export const userRegisterValidate = z.object({
     username:z.string().min(1,{message: "This field cannot be empty"}),
     email : z.string().email(),
-    password: z.string().min(8, {message: "password should be atleast 6 characters long"})
+    password: z.string().min(8, {message: "password should be atleast 6 characters longg"})
 })
